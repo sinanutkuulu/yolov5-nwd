a5cc486114ac7a863781f246e7c9cf6eb6a6beea
Sinan Utku Ulu <ulu@192.168.1.92>
2024-02-08 19:00:52 +0300

    nms

--- utils/general.py
+++ utils/general.py
@@ -982,8 +982,8 @@ def non_max_suppression(
             # Batched NMS
         c = x[:, 5:6] * (0 if agnostic else max_wh)  # classes
         boxes, scores = x[:, :4] + c, x[:, 4]  # boxes (offset by class), scores
-        i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
-        #i = nwd_based_nms(boxes, scores)  # NMS
+        #i = torchvision.ops.nms(boxes, scores, iou_thres)  # NMS
+        i = nwd_based_nms(boxes, scores)  # NMS
         i = i[:max_det]  # limit detections
         if merge and (1 < n < 3E3):  # Merge NMS (boxes merged using weighted mean)
                 # update boxes as boxes(i,4) = weights(i,n) * boxes(n,4)
