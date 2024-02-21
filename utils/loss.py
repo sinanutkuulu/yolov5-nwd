5aa8e713ecd9c7305909b9873115dba38b23d109
Sinan Utku Ulu <ulu@192.168.1.80>
2024-02-21 03:25:39 +0300

    loss scale

--- utils/loss.py
+++ utils/loss.py
@@ -139,7 +139,7 @@ class ComputeLoss:
                 pwh = (pwh.sigmoid() * 2) ** 2 * anchors[i]
                 pbox = torch.cat((pxy, pwh), 1)  # predicted box
                 #iou = bbox_iou(pbox, tbox[i], CIoU=True).squeeze()  # iou(prediction, target)
-                iou = bbox_overlaps_nwd(pbox, tbox[i]).squeeze()
+                iou = bbox_overlaps_nwd(pbox, tbox[i]).squeeze() * 10
                 lbox += (1.0 - iou).mean()  # iou loss
 
                 # Objectness
